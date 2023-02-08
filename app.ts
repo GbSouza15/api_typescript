676c897deacc6b44b7d5d40f8704b3f66e2f168f
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-02-08 20:08:09 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,43 @@
+const input = document.getElementById('camp-search') as HTMLInputElement
+const btn = document.getElementById('btn-search') as HTMLButtonElement
+const cards = document.querySelector('.list-card') as HTMLUListElement
+
+function createCards(response: Array<object>) {
+    if(response.length == 0) {
+        alert('Não foi possível encontrar uma resposta!')
+    }
+
+    response.map((item: any) => {
+        const createLi = document.createElement('li')
+
+        createLi.innerHTML = `
+            <div class="card-header">
+                    <h3>${item.title}</h3>
+                </div>
+                <div class="card-body">
+                    <p>${item.snippet}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="https://en.wikipedia.org/?curid=${item.pageid}" target="_blank">Leia mais...</a>
+                </div>
+        `
+        cards.appendChild(createLi)
+    })
+}
+
+btn.addEventListener('click', (e) => {
+    cards.innerHTML = ''
+
+    e.preventDefault()
+
+    let srSearch = input.value
+    const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${srSearch}&format=json`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let response = data.query.search
+            createCards(response)
+            console.log(response)
+        })
+})
